b1dfcba53e322f01cb25dbb8f522c323a375901c
meghanasarikonda <meghana94987@gmail.com>
2018-03-29 15:08:54 +0530

    Components and images

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>Hi, I'm Main Component</div>
+    );
+  }
+}
+
+export default Main;
