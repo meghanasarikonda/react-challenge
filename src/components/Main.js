d17f3b01e724812c19792134a25aaf3db062b36c
meghanasarikonda <meghana94987@gmail.com>
2018-03-29 17:49:36 +0530

    Basic layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import imageObj from '../Imagefiles.js';
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    console.log(imageObj["avatar1"])
     return (
-      <div>Hi, I'm Main Component</div>
+      <div>
+        <img src={imageObj["avatar1"]}/>
+        <div>
+          Here comes popover
+        </div>
+      </div>
     );
   }
 }
