f047b1f5ae970bbe0af056e95643b14e38ad06ec
meghanasarikonda <meghana94987@gmail.com>
2018-03-29 17:48:10 +0530

    Import and export images

---
+++ src/Imagefiles.js
@@ -0,0 +1,23 @@
+import avatar1 from './Images/avatar1.png';
+import avatar2 from './Images/avatar2.png';
+import avatar3 from './Images/avatar3.png';
+import avatar4 from './Images/avatar4.png';
+import avatar5 from './Images/avatar5.png';
+import avatar6 from './Images/avatar6.png';
+
+// export avatar1 as avatar1;
+// export avatar2 as avatar2;
+// export avatar3 as avatar3;
+// export avatar4 as avatar4;
+// export avatar5 as avatar5;
+// export avatar6 as avatar6;
+
+
+export default {
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6
+}
