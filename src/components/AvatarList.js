70b910dfeeada834ac6fb465e079fa853508112c
meghanasarikonda <meghana94987@gmail.com>
2018-03-30 15:29:04 +0530

    Can select avatars to update

--- src/components/AvatarList.js
+++ src/components/AvatarList.js
@@ -5,20 +5,32 @@ class AvatarList extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       obj: this.props.imageObj
     }
+
+    this.handleClick = (i, event) => {
+      // console.log('cool')
+      // console.log(event.target, 'event')
+      console.log(i, 'i line 19')
+      console.log(event.target, 'event target')
+      this.props.imageUpdater(i);
+    }
+
+
   }
 
+
   render() {
-    console.log(this.state.obj, 'AvatarList')
+    // console.log(this.state.obj, 'AvatarList')
     return (
       <div>
         <div className="triangle"></div>
         <div className="popover">
           <h1 className="title">Choose your avatar</h1>
           {this.state.obj.map((imageObj) =>
-            <img src={imageObj["src"]} key={imageObj["id"]} alt={imageObj["label"]} />
+            <img className="avatarlist" src={imageObj["src"]} key={imageObj["id"]} alt={imageObj["label"]} onClick={this.handleClick.bind(this, imageObj["id"])} />
           )}
         </div>
       </div>
