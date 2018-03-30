4fdbec9368ee60568432705b37a04891b657878c
meghanasarikonda <meghana94987@gmail.com>
2018-03-30 15:29:32 +0530

    Render selected avatar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import imageObj from '../Imagefiles.js';
+import ImageList from '../Imagefiles.js';
 import SingleAvatar from './SingleAvatar'
 import AvatarList from './AvatarList'
 
@@ -8,16 +8,25 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: imageObj["avatar1"],
-      imageObj: imageObj
+      image: ImageList[0],
+      ImageList: ImageList,
+      initial: true
+    }
+
+    this.imageUpdater = (id) => {
+      //console.log(e, 'e from line 16')
+      this.setState({
+        image: ImageList[id - 1]
+      });
     }
   }
 
   render() {
+    console.log(this.state.image, "line24")
     return (
       <div>
-        <SingleAvatar image={this.state.image}/>
-        <AvatarList />
+        <SingleAvatar image={this.state.image} />
+        <AvatarList imageObj={this.state.ImageList} imageUpdater={this.imageUpdater} initial={this.state.initial} />
       </div>
     );
   }
