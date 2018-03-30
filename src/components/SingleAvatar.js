2f9496be49e62ad081bc3caf6bdd580dbe9a0e32
meghanasarikonda <meghana94987@gmail.com>
2018-03-30 15:28:22 +0530

    Render single avatar

--- src/components/SingleAvatar.js
+++ src/components/SingleAvatar.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import './SingleAvatar.css';
 
 class SingleAvatar extends Component {
-
   constructor(props) {
     super(props);
+
+    this.state = {
+      image: this.props.image,
+      initial: true
+    }
+
+    this.handleClick = () => {
+
+    }
   }
 
+
   render() {
-    //console.log('props', this.props)
+    console.log('state', this.state.image)
+    console.log('props', this.props.image)
+
+    // const image = this.state.image
+    // const img = this.state.initial ? (
+    //     <img className="avatar" src={this.state.image["src"]} key={this.state.image["id"]} alt={this.state.image["label"]} onClick={() => console.log('hurray')} onClick={this.handleClick} />
+    // ) : (
+    //   <h1>Here</h1>
+    // )
+
     return (
       <div>
-      <a href="#" onClick={() => console.log('hurray')}>
-        <img className="avatar" src={this.props.image}/>
-      </a>
+        <img className="avatar" src={this.props.image["src"]} key={this.props.image["id"]} alt={this.props.image["label"]} onClick={() => console.log('hurray')} onClick={this.handleClick} />
       </div>
     );
   }
