5cd5ad6fea8ec375072eddca790bad09c292c5bd
meghanasarikonda <meghana94987@gmail.com>
2018-04-11 23:54:47 +0530

    BEM naming convention

--- src/components/AvatarList.js
+++ src/components/AvatarList.js
@@ -71,19 +71,19 @@ class AvatarList extends Component {
 
     return (
       <div>
-        <div className={this.state.popover ? "triangle" : ""}></div>
-        <div className={this.state.popover ? "popover" : ""} >
-          <h1 className="title">Choose your avatar</h1>
+        <div className={this.state.popover ? "Avatarlist-triangle" : ""}></div>
+        <div className={this.state.popover ? "Avatarlist-popover" : ""} >
+          <h1 className="Avatarlist-title">Choose your avatar</h1>
           <ul ref={node => this.node = node}>
 
             {
               this.state.obj.map((imageObj) => (
                 <li
                   key={100 - imageObj["id"]}
-                  className={this.state.isLoading ? (imageObj["id"] === this.state.loadingId  ? "avatarlistload spinner " : "avatarlist") : (imageObj["id"] === this.props.image["id"] ? "currentavatar": "avatarlist")}
+                  className={this.state.isLoading ? (imageObj["id"] === this.state.loadingId  ? "Avatarlist-avatarlistload spinner " : "Avatarlist-avatarlist") : (imageObj["id"] === this.props.image["id"] ? "Avatarlist-currentavatar": "Avatarlist-avatarlist")}
                   onClick={this.handleClick.bind(this, imageObj["id"])}>
 
-                  <div className={this.state.isLoading ? "": "image"}>
+                  <div className={this.state.isLoading ? "": "Avatarlist-image"}>
                     <img
                       src={imageObj["src"]}
                       key={imageObj["id"]}
